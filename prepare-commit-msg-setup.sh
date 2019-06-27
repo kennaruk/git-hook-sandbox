e495b14adc977196bdca4de6a06fc12b5f7e8a76
Nitipat Wuttisasiwat (Ken) <nitipat.wuttisasiwat@refinitiv.com>
2019-06-27 19:53:13 +0700

    setup sh fixed

---
+++ prepare-commit-msg-setup.sh
@@ -0,0 +1,3 @@
+curl -O https://raw.githubusercontent.com/kennaruk/git-hook-sandbox/master/prepare-commit-msg/prepare-commit-msg.sh
+chmod +x prepare-commit-msg.sh
+mv prepare-commit-msg.sh .git/hooks/prepare-commit-msg.sh
